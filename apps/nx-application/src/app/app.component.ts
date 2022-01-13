c494c3a5eec653c3ba62f8109ec4e341358f77ac
BhadraGopi <96776676+BhadraGopi@users.noreply.github.com>
2022-01-13 15:43:47 +0530

    done

--- apps/nx-application/src/app/app.component.ts
+++ apps/nx-application/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nx-application';
+  isShow = false;
+  showMenu(): void {
+    this.isShow = !this.isShow;
+  }
 }
