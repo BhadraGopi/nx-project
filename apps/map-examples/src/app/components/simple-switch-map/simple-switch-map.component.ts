c8083bac34e7c1406e47a90df86f35aa3266cecc
BhadraGopi <96776676+BhadraGopi@users.noreply.github.com>
2022-01-17 16:32:07 +0530

    done

--- apps/map-examples/src/app/components/simple-switch-map/simple-switch-map.component.ts
+++ apps/map-examples/src/app/components/simple-switch-map/simple-switch-map.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { timer, interval } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { map, merge, mergeMap, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { concatMap } from 'rxjs';
 @Component({
   selector: 'nx-project-simple-switch-map',
   templateUrl: './simple-switch-map.component.html',
@@ -41,6 +42,35 @@ export class SimpleSwitchMapComponent implements OnInit {
       )
       .subscribe((ret) => {
         console.log('Recd ' + ret);
+      });
+    const srcObservableC = of(1, 2, 3, 4);
+    const innerObservableC = of('A', 'B', 'C', 'D');
+
+    srcObservableC
+      .pipe(
+        concatMap((val) => {
+          console.log('Source value ' + val);
+          console.log('starting new observable');
+          return innerObservableC;
+        })
+      )
+      .subscribe((ret) => {
+        console.log('Recd Concat ' + ret);
+      });
+
+    const srcObservableM = of(1, 2, 3, 4);
+    const innerObservableM = of('A', 'B', 'C', 'D');
+
+    srcObservable
+      .pipe(
+        mergeMap((val) => {
+          console.log('Source value ' + val);
+          console.log('starting new observable');
+          return innerObservable;
+        })
+      )
+      .subscribe((ret) => {
+        console.log('Recd M ' + ret);
       });
   }
 }
