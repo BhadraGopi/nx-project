d40e465634692b2ed44bbb0b420087250674c4dd
BhadraGopi <96776676+BhadraGopi@users.noreply.github.com>
2022-01-20 17:27:37 +0530

    done

--- apps/store-state-example/src/app/app.module.ts
+++ apps/store-state-example/src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppComponent } from './app.component';
 import { counterReducer } from './counter.reducer';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { MyCounterComponent } from './my-counter/my-counter/my-counter.component';
+import { MyMoviesComponent } from './examples/my-movies/my-movies.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, MyCounterComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    MyCounterComponent,
+    MyMoviesComponent,
+  ],
   imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/store-state-example/src/app/examples/my-movies/my-movies.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nx-project-my-movies',
+  templateUrl: './my-movies.component.html',
+  styleUrls: ['./my-movies.component.css'],
+})
+export class MyMoviesComponent implements OnInit {
+  constructor() {
+    //donothing
+  }
+
+  ngOnInit(): void {
+    console;
+  }
+}
